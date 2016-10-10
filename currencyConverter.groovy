192016f0e11955a32619728aee006915e33e1e50
Ms Sarah Vincent <svince04@dcs.bbk.ac.uk>
2016-10-10 18:46:19 +0100

    Changed output message:wq

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -18,7 +18,7 @@ while (!finished) {
 	  print "How many pounds would you like to convert? ";
 	  double pounds = Double.parseDouble(System.console().readLine());
 	  double euro   = pounds * euroOverPoundRatio;
-	  println "£" + pounds + " will give you " + euro + "€";
+	  println "£" + pounds + " will give you " + euro + "€" + " (i.e., less than a week ago)";
 	  break;
     case 2: 
 	  print "How many euro would you like to convert? ";
